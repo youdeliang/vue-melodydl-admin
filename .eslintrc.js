c436459b52d83f5ebe4f2d37ef1626d36cf1cccd
youdeliang <1048726003@qq.com>
2020-10-27 00:33:15 +0800

    Initial commit.

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    node: true
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "@vue/prettier",
+    "@vue/prettier/@typescript-eslint"
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    '@typescript-eslint/no-var-requires': 'off'
+  }
+};
